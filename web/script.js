0bf7a726c230758b2cf077de9728e6a03f4d2b8a
vladn79 <vladdd.nov@gmail.com>
2023-12-23 15:06:03 +0200

    added transition between start and select menu

--- web/script.js
+++ web/script.js
@@ -9,9 +9,11 @@ function animateDiagonal() {
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
   
-      const duration = 1500;
+      const lineDuration = 1500;
+      const fadeDuration = 500; 
       const lineWidth = 10;
-      let startTime;
+      let lineStartTime;
+      let fadeStartTime;
   
       function drawLine(progress) {
           context.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -23,20 +25,33 @@ function animateDiagonal() {
           context.stroke();
       }
   
-      function animate(time) {
-          if (!startTime) startTime = time;
-          const progress = Math.min(1, (time - startTime) / duration);
+      function animateLine(time) {
+          if (!lineStartTime) lineStartTime = time;
+          const lineProgress = Math.min(1, (time - lineStartTime) / lineDuration);
   
-          drawLine(progress);
+          drawLine(lineProgress);
   
-          if (progress < 1) {
-              requestAnimationFrame(animate);
+          if (lineProgress < 1) {
+              requestAnimationFrame(animateLine);
+          } else {
+              fadeStartTime = performance.now();
+              requestAnimationFrame(animateFade);
+          }
+      }
+  
+      function animateFade(time) {
+          const fadeProgress = Math.min(1, (time - fadeStartTime) / fadeDuration);
+          contContainer.style.opacity = 1 - fadeProgress;
+          menuContainer.style.opacity = fadeProgress;
+  
+          if (fadeProgress < 1) {
+              requestAnimationFrame(animateFade);
           } else {
               contContainer.style.display = 'none';
               menuContainer.style.display = 'block';
           }
       }
   
-      requestAnimationFrame(animate);
+      requestAnimationFrame(animateLine);
   }
   
